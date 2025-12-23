8eab137c54cb320f30a1bc1b0951497dcc588f59
parvejtalukder <mail@parvejhusentalukder.com>
2025-12-24 01:03:23 +0600

    Update: FInal COmmit

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 const port = process.env.PORT || 3001;
 const site = process.env.SITE_NAME;
 const admin = require("firebase-admin");
-const serviceAccount = require("./artifyFirebase.json");
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
